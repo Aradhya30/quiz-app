0632a2eefab31ef1639d08b21a0040eee3038c6b
Aradhya Patil <aradhyapatil2006@gmail.com>
2026-03-31 16:19:44 +0530

    quiz app with 10 questions

---
+++ script.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    answers: ["Hyper Trainer Marking Language", "Hyper Text Markup Language", "Hyper Text Marketing Language", "Hyper Tool Markup Language"],
+    correct: 1
+  },
+  {
+    question: "Which language is used for styling web pages?",
+    answers: ["HTML", "JQuery", "CSS", "XML"],
+    correct: 2
+  },
+  {
+    question: "Which is not a JavaScript framework?",
+    answers: ["Python Script", "React", "Angular", "Vue"],
+    correct: 0
+  },
+  {
+    question: "Which is used for web development?",
+    answers: ["HTML", "CSS", "JavaScript", "All of the above"],
+    correct: 3
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: ["Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheets", "Colorful Style Sheets"],
+    correct: 2
+  },
+  {
+    question: "Inside which HTML element do we put JavaScript?",
+    answers: ["<js>", "<javascript>", "<script>", "<code>"],
+    correct: 2
+  },
+  {
+    question: "Which company developed JavaScript?",
+    answers: ["Microsoft", "Netscape", "Google", "IBM"],
+    correct: 1
+  },
+  {
+    question: "Which HTML attribute is used to define inline styles?",
+    answers: ["font", "class", "style", "styles"],
+    correct: 2
+  },
+  {
+    question: "Which property is used to change background color in CSS?",
+    answers: ["color", "bgcolor", "background-color", "background"],
+    correct: 2
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    answers: ["//", "<!-- -->", "#", "**"],
+    correct: 0
+  }
+];
